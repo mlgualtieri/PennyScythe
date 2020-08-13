064c261d07d214904ec4392e8b2b7fe19b0e7542
Mike Gualtieri <mike.gualtieri@gmail.com>
2020-08-12 22:44:05 -0400

    many enhancements; now supports multiple threats

--- PennyScythe.py
+++ PennyScythe.py
@@ -1,52 +1,141 @@
 #!/usr/bin/env python3
 #
 # PennyScythe.py is a Python3 script that ingests and runs adversarial emulation
-# plans from SCYTHE's Community Threats Repository 
-# [https://github.com/scythe-io/community-threats]
+# plans from SCYTHE's Community Threats Repository.  Its goal is to aid in 
+# Purple Teaming exercises.  Its functionality is not limited to SCYTHE's
+# Community Threats, and will support any modules developed with similar syntax.
+#
+# https://github.com/scythe-io/community-threats
 # 
-# The script has only been tested with the EvilCorp WastedLocker threat plan, 
-# and would likely require enhancements to work with other plans
+# Many of SCYTHE's Community Threats work with this script.  Support status is
+# indicated for each module with --list.
 #
-# Usage: python[.exe] PennyScythe.py
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#
-# DISCLAIMER: I don't work for Scythe and this project is in no way associated 
-# with or endorsed by scythe.io.  Use at your own risk!
+# Usage: python[.exe] PennyScythe.py --t EvilCorp/EvilCorp-WastedLocker -r
 #
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
-# Author:   Mike Gualtieri
-# Blog:     https://www.mike-gualtieri.com
-# Twitter:  https://twitter.com/mlgualtieri
-# GitHub:   https://github.com/mlgualtieri/PennyScythe
+# DISCLAIMER: I don't work for SCYTHE and this project is in no way associated 
+# with or endorsed by scythe.io.  Use at your own risk!  Unicorns welcome!
+#
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#
+# Author  : Mike Gualtieri
+# Blog    : https://www.mike-gualtieri.com
+# Twitter : https://twitter.com/mlgualtieri
+# GitHub  : https://github.com/mlgualtieri/PennyScythe
 #
 
-import os, time, sys, urllib.request, json, shutil
-
-# Emulate EvilCorp WastedLocker
-EMULATION_PLAN_URL = "https://raw.githubusercontent.com/scythe-io/community-threats/master/EvilCorp/WastedLocker_scythe_threat.json"
-
-# Default time to sleep between steps in plan
-DEFAULT_SLEEP      = 5
-
-# Likely only useful to your author 
-DEBUG              = False
+import os, time, sys, urllib.request, json, shutil, textwrap, getopt
 
 
-def doRun(request):
+# APT19/APT19                       (partial support - no printsreen)
+# APT33/APT33                       (not supported)
+# Buhtrap/Buhtrap-DNS               (not supported)
+# Buhtrap/Buhtrap-HTTPS             (not supported)
+# CozyBear/CozyBear-Step1           (supported)
+# CozyBear/CozyBear-Step2           (partial support - no uploader, time delay)
+# DeepPanda/Deep_Panda_Desrubi      (supported)
+# Orangeworm/Orangeworm             (supported)
+# Ransomware/Ransomware_Example     (supported)
+# EvilCorp/EvilCorp-WastedLocker    (supported)
+
+threats = {
+    "APT19/APT19": {
+        "display_name": "APT19 for #ThreatThursday",
+        "description": "APT19",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/APT19/APT19_scythe_threat.json",
+        "support": "Partial Support"
+    },
+    "APT33/APT33": {
+        "display_name": "APT33",
+        "description": "APT33 is a suspected Iranian threat group that has carried out operations since at least 2013.",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/APT33/APT33_scythe_threat.json",
+        "support": "Not Supported"
+    },
+    "Buhtrap/Buhtrap-DNS": {
+        "display_name": "Buhtrap-DNS",
+        "description": "Long-haul C2 over DNS for the persistence portion of Buhtrap Adversary Emulation",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/Buhtrap/Buhtrap-DNS_scythe_threat.json",
+        "support": "Not Supported"
+    },
+    "Buhtrap/Buhtrap-HTTPS": {
+        "display_name": "Buhtrap-HTTPS",
+        "description": "Short-haul C2 over HTTPS for the collection portion of Buhtrap Adversary Emulation",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/Buhtrap/Buhtrap-HTTPS_scythe_threat.json",
+        "support": "Not Supported"
+    },
+    "CozyBear/CozyBear-Step1": {
+        "display_name": "CozyBear-Step1",
+        "description": "Cozy Bear Step 1 of the MITRE ATT&CK Evaluations Adversary Emulation Plan",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/CozyBear/CozyBear-Step1_scythe_threat.json",
+        "support": "Supported"
+    },
+    "CozyBear/CozyBear-Step2": {
+        "display_name": "CozyBear-Step2",
+        "description": "Cozy Bear Step 2 of the MITRE ATT&CK Evaluations Adversary Emulation Plan",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/CozyBear/CozyBear-Step2_scythe_threat.json",
+        "support": "Partial Support"
+    },
+    "DeepPanda/Deep_Panda_Desrubi": {
+        "display_name": "Deep Panda Desrubi",
+        "description": "Linux Variant of Desrubi",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/DeepPanda/Deep_Panda_Desrubi_scythe_threat.json",
+        "support": "Supported"
+    },
+    "EvilCorp/EvilCorp-WastedLocker": {
+        "display_name": "WastedLocker",
+        "description": "WastedLocker is the ransomware that was used by EvilCorp in the July 2020 attack against Garmin.",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/EvilCorp/WastedLocker_scythe_threat.json",
+        "support": "Supported"
+    },
+    "Orangeworm/Orangeworm": {
+        "display_name": "Orangeworm",
+        "description": "Orangeworm is a group that has targeted organizations in the healthcare sector in the United States, Europe, and Asia since at least 2015.",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/Orangeworm/Orangeworm_scythe_threat.json",
+        "support": "Supported"
+    },
+    "Ransomware/Ransomware_Example": {
+        "display_name": "Ransomware Example",
+        "description": "This Threat Template provides an example ransomware attack, but instead of using actual user data, it creates a directory with files specifically for the purpose of encrypting them. It then downloads a ransom note from the public internet.",
+        "url": "https://raw.githubusercontent.com/scythe-io/community-threats/master/Ransomware/Ransomware_Example_scythe_threat.json",
+        "support": "Supported"
+    }
+}
+
+
+
+def listThreats():
+    print('')
+    print("Available Modules:")
+    for threat in threats:
+        print("\n  [*] ", threat, sep='')
+        print("\tName        :", threats[threat]['display_name'])
+        print("\tStatus      :", threats[threat]['support'])
+
+        # Format/output description
+        desc_out = "Description : " + threats[threat]['description']
+        desc_wrapper = textwrap.TextWrapper(width=80, initial_indent=' ' * 8, subsequent_indent=' ' * 8) 
+        desc_out = desc_wrapper.fill(text=desc_out) 
+        print(desc_out)
+
+    sys.exit()
+
+
+
+
+def doRun(request, default_sleep, debug):
     print("         > Running:", request)
-    time.sleep(DEFAULT_SLEEP)
+    time.sleep(default_sleep)
     ### Debug
-    if DEBUG == True:
-        request = "ls /tmp"
+    if debug == True:
+        request = "echo OK"
     os.system(request)
 
 
 
-def doFile(request):
+def doFile(request, default_sleep, debug):
     print("         > File:", request)
-    time.sleep(DEFAULT_SLEEP)
+    time.sleep(default_sleep)
 
     req  = request.split(" ")
 
@@ -55,10 +144,12 @@ def doFile(request):
         size  = req[ req.index("--size")  + 1]
         count = int(req[ req.index("--count") + 1])
 
-        path  = path.replace("%USERPROFILE%", os.environ["USERPROFILE"])
+        # Only specify for Windows
+        if "USERPROFILE" in os.environ:
+            path = path.replace("%USERPROFILE%", os.environ["USERPROFILE"])
 
         ### Debug
-        if DEBUG == True:
+        if debug == True:
             path = "/tmp/scythe/important_files.wasted"
 
         # Convert size to bytes
@@ -83,18 +174,20 @@ def doXOR(data, key):
 
 
 # We'll use XOR encryption since it's simple to implement
-def doCrypt(request):
+def doCrypt(request, default_sleep, debug):
     print("         > Crypt:", request)
-    time.sleep(DEFAULT_SLEEP)
+    time.sleep(default_sleep)
 
     req  = request.split(" ")
 
     target    = req[ req.index("--target")  + 1].strip('\"')
     password  = req[ req.index("--password")  + 1].strip('\"')
 
-    target    = target.replace("%USERPROFILE%", os.environ["USERPROFILE"])
+    # Only specify for Windows
+    if "USERPROFILE" in os.environ:
+        target = target.replace("%USERPROFILE%", os.environ["USERPROFILE"])
 
-    if DEBUG == True:
+    if debug == True:
         target = "/tmp/scythe"
 
     doErase   = False
@@ -126,21 +219,24 @@ def doCrypt(request):
 
 
 
-def doDownloader(request):
+def doDownloader(request, default_sleep, debug):
     print("         > Download :", request)
-    time.sleep(DEFAULT_SLEEP)
+    time.sleep(default_sleep)
 
     req  = request.split(" ")
     src  = req[ req.index("--src")  + 1].strip('\"')
     dest = req[ req.index("--dest") + 1].strip('\"')
 
-    dest = dest.replace("%USERPROFILE%", os.environ["USERPROFILE"])
+    # Only specify for Windows
+    if "USERPROFILE" in os.environ:
+        dest = dest.replace("%USERPROFILE%", os.environ["USERPROFILE"])
     
-    # Make sure we pull the "raw" file
-    src  = src.replace("pastebin.com/","pastebin.com/raw/")
+    # If URL is from pastebin, make sure we pull the "raw" file
+    if "pastebin.com/raw/" not in src:
+        src  = src.replace("pastebin.com/","pastebin.com/raw/")
 
     ### Debug
-    if DEBUG == True:
+    if debug == True:
         dest = "/tmp/scythe/wasted_info.txt"
 
     print("            Source :", src)
@@ -157,9 +253,9 @@ def doDownloader(request):
 
 
 
-def doController(request):
+def doController(request, default_sleep, debug):
     print("         > Controller :", request)
-    time.sleep(DEFAULT_SLEEP)
+    time.sleep(default_sleep)
 
     if request == "--shutdown":
         print("\n\nController is requesting to initiate a shutdown. Proceed? [y/N] ", end="")
@@ -167,7 +263,7 @@ def doController(request):
         
         if theInput == "y":
             print("Shutting down...")
-            time.sleep(DEFAULT_SLEEP)
+            time.sleep(default_sleep)
             os.system("shutdown /s /t 0")
         else:
             print("Skipping shutdown...")
@@ -175,24 +271,47 @@ def doController(request):
 
 
 
-def startEmulation(emulation_plan_url):
+def startEmulation(emulation_plan_url, default_sleep, test_threat, debug):
+    # Keep track of unsupported modules
+    unsupported = 0
+
     with urllib.request.urlopen(emulation_plan_url) as url:
         emulation_plan = json.loads(url.read().decode())
     
-    
+    # Format/output description
+    desc_out = "Description : " + emulation_plan["threat"]["description"]
+    desc_wrapper = textwrap.TextWrapper(width=80, initial_indent='', subsequent_indent=' ' * 14)
+    desc_out = desc_wrapper.fill(text=desc_out) 
+
     print("Threat      :", emulation_plan["threat"]["display_name"])
-    print("Description :", emulation_plan["threat"]["description"])
     print("Platform    :", emulation_plan["threat"]["operating_system_name"].capitalize())
+    print(desc_out)
+
+    # Platform test
+    if debug is False:
+        if emulation_plan["threat"]["operating_system_name"] == "windows" and os.name != 'nt':
+            print("\n[!] Error: This threat requires Windows")
+            sys.exit()
+        elif emulation_plan["threat"]["operating_system_name"] == "linux" and os.name != 'posix':
+            print("\n[!] Error: This threat requires Linux")
+            sys.exit()
+
     
     print('')
-    print("\nReady to start? [Y/n] ", end="")
+    if test_threat == False:
+        print("\nReady to start? [Y/n] ", end="")
+    else:
+        print("\nReady to test? [Y/n] ", end="")
+
+
+    # Check for Y/n
     doStart = input().lower()
-    
     if doStart == "n":
-        exit()
+        sys.exit()
     
     
-    print("\nStarting emulation...")
+    if test_threat == False:
+        print("\nStarting emulation...")
     
     for i in emulation_plan["threat"]["script"]:
         step = emulation_plan["threat"]["script"][i]
@@ -201,28 +320,50 @@ def startEmulation(emulation_plan_url):
             if step["type"] == "message":
     
                 # Skip over initialization and loader steps
-                valid_modules = ["run","file","crypt","downloader","controller"]
+                valid_modules = ["run","file","crypt","downloader","controller", "loader"]
     
                 if step["module"] in valid_modules:
     
-                    if "rtags" in step:
-                        for tag in step["rtags"]:
-                            _tag = tag.split(":")
-                            if(_tag[0] == "att&ck-technique"):
-                                print("\n    Emulating ATT&CK Technique : ", _tag[1], " [https://attack.mitre.org/techniques/", _tag[1] ,"]", sep = "")
+                    if test_threat == False:
+                        if "rtags" in step:
+                            for tag in step["rtags"]:
+                                _tag = tag.split(":")
+                                if(_tag[0] == "att&ck-technique"):
+                                    print("\n    Emulating ATT&CK Technique : ", _tag[1], " [https://attack.mitre.org/techniques/", _tag[1] ,"]", sep = "")
                     
                     if "module" in step:
-                        if step["module"] == "run":
-                            doRun(step["request"])
-                        elif step["module"] == "file":
-                            doFile(step["request"])
-                        elif step["module"] == "crypt":
-                            doCrypt(step["request"])
-                        elif step["module"] == "downloader":
-                            doDownloader(step["request"])
-                        elif step["module"] == "controller":
-                            doController(step["request"])
+                        # Ignore 'loader'
+                        if step["module"] != "loader":
+                            if test_threat == True:
+                                if debug == True:
+                                    print("Module", step["module"], ": Supported...")
+                            else:
+                                if step["module"] == "run":
+                                    doRun(step["request"], default_sleep, debug)
+                                elif step["module"] == "file":
+                                    doFile(step["request"], default_sleep, debug)
+                                elif step["module"] == "crypt":
+                                    doCrypt(step["request"], default_sleep, debug)
+                                elif step["module"] == "downloader":
+                                    doDownloader(step["request"], default_sleep, debug)
+                                elif step["module"] == "controller":
+                                    doController(step["request"], default_sleep, debug)
     
+                else:
+                    unsupported += 1
+                    print("[!] Warning: Module", step["module"], "is not supported...")
+
+    if test_threat == True:
+        if unsupported == 0:
+            print('')
+            print("Awesome! This threat appears to be fully supported!")
+            print('')
+        else:
+            print('')
+            print("Bummer. This threat is not fully supported.")
+            print('')
+
+
     print("Done!")
 
 
@@ -273,23 +414,106 @@ def banner():
     print('                            .:/osyddmNNNNNNmmdhys+:.`                           ')
     print('')
     print('                               -= PennyScythe =-')
-    print('                     -= Adversarial Emulation on the Cheap =-')
+    print('               -= Adversarial Emulation for a Penny or Less! =-')
     print('')
-    print('Note: This project is in no way associated with or endorsed by scythe.io!')
+    print('This project is in no way associated with or endorsed by SCYTHE [scythe.io]!')
+
+
+
+
+# Print basic usage
+def usage():
+    print('')
+    print('usage: PennyScythe.py -t <threat> -r [-u <url>] [-s 5] [-c] [-d] [-h] [-l] [-r]')
+    print('')
+
+
+# Display verbose help
+def showhelp():
+    usage()
+    print('Main options:')
+    print('  -c, --compat           Test the specified --threat or --url')
+    print('                         for compatibility')
+    print('  -d, --debug            Turn on debug options likely only useful')
+    print('                         to the author of this script')
+    print('  -h, --help')
+    print('  -l, --list             List all available threat modules')
+    print('  -r, --run              Run the specified --threat or --url')
+    print('  -s, --sleep            Sleep time in seconds between steps of the')
+    print('                         specified --threat or --url')
+    print('  -t, --threat <threat>  Specify a threat to test --compat or --run')
+    print('  -u, --url    <url>     Supply a URL to a prepared threat plan')
+    print()
+
 
 
 
 
 def main(argv):
-    try:
-        banner()
-        print('')
-        startEmulation(EMULATION_PLAN_URL)
-    except KeyboardInterrupt:
-        # Gracefully exit on ctrl-c
-        print("\nBye!")
-        pass
 
+    # Check to see if command line args were sent
+    if not argv:
+        banner()
+        usage()
+        sys.exit()
+
+    # Default vars
+    debug               = False
+    default_sleep       = 5
+    threat              = ""
+    run_threat          = False
+    test_threat         = False
+    emulation_plan_url  = ""
+
+    # Process command line args
+    try:
+        opts, args = getopt.getopt(argv,"cdhlrs:t:u:",["compat","debug","help","list","run","sleep=","threat=","url="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h','--help'):
+            banner()
+            showhelp()
+            sys.exit()
+        elif opt in ("-d", "--debug"):
+            debug = True
+        elif opt in ("-l", "--list"):
+            banner()
+            listThreats()
+            sys.exit()
+        elif opt in ("-t", "--threat"):
+            threat = arg
+        elif opt in ("-u", "--url"):
+            emulation_plan_url = arg
+        elif opt in ("-s", "--sleep"):
+            default_sleep = int(arg)
+        elif opt in ("-r", "--run"):
+            run_threat = True
+        elif opt in ("-c", "--compat"):
+            test_threat = True
+        else:
+            usage()
+            sys.exit(2)
+
+
+    # Set our --threat
+    if threat in threats:
+        if "url" in threats[threat]:
+            emulation_plan_url = threats[threat]['url']
+
+
+    if (run_threat == True or test_threat == True) and emulation_plan_url != "":
+        try:
+            banner()
+            print('')
+            startEmulation(emulation_plan_url, default_sleep, test_threat, debug)
+        except KeyboardInterrupt:
+            # Gracefully exit on ctrl-c
+            print("\nBye!")
+            pass
+    else:
+        usage()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
